3bb6c5fda63b7a09c9e330f692fe0943ef0f38dc
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-09-13 23:13:20 -0300

    criados os componentes basicos

---
+++ src/components/signin.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function SignIn () {
+
+    return (
+    <Page>
+    SIGN IN
+    </Page>   
+    )
+}
+
+const Page = styled.div`
+    background-color: #blue;
+`
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import Site from "./components/site";
+import "./css/reset.css";
+
+ReactDOM.render(<Site />, document.querySelector(".root"));
